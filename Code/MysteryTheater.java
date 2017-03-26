335b3e4fbad21e171e73ab74527c5200ceefaa09
mhauck3 <mhauck3@uic.edu>
2017-03-26 17:26:02 -0500

    Update MysteryTheater.java

--- Code/MysteryTheater.java
+++ Code/MysteryTheater.java
@@ -1,13 +1,18 @@
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
-import java.io.BufferedReader;
+//import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class MysteryTheater extends JFrame implements GameInfo{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -952184984365619687L;
+
 	public MysteryTheater(){
 		initUI();
 	}
@@ -28,10 +33,27 @@ public class MysteryTheater extends JFrame implements GameInfo{
 		try {
 			FileReader reader = new FileReader(file);
 			int resolution = reader.read();
+			char moveLeftMapping = (char) reader.read();
+			char moveRightMapping = (char) reader.read();
+			char moveUpMapping = (char) reader.read();
+			char moveDownMapping = (char) reader.read();
+			char attackOneMapping = (char) reader.read();
+			char attackTwoMapping = (char) reader.read();
+			System.out.println(" move left mapping = " + moveLeftMapping);
+			System.out.println(" move right mapping = " + moveRightMapping);
+			System.out.println(" move up mapping = " + moveUpMapping);
+			System.out.println(" move right mapping = " + moveDownMapping);
+			System.out.println(" attack one mapping = " + attackOneMapping);
+			System.out.println(" attack two mapping = " + attackTwoMapping);
+			//could let player choose chars for mapping
+			//by updating GameLauncher by adding to GUI
+			//hitting any key needs to go to event handler
+			//doing mapping: "if key pressed
+
 			Global.size = resolution;
 			reader.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			//TODO Auto-generated catch block
 			//e.printStackTrace();
 			//GameLauncher gl = new GameLauncher();
 			System.exit(-1);
@@ -39,17 +61,17 @@ public class MysteryTheater extends JFrame implements GameInfo{
 	}
 	
 	public static void initGame(){
-		readFile();		
+		readFile();
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				JFrame ex = new MysteryTheater();
-				ex.setVisible(true);	
+				ex.setVisible(true);
 			}
-		});		
+		});
 	}
 	
-	public static void main(String[] args){	
+	public static void main(String[] args) {	
 		initGame();
 	}
 }
