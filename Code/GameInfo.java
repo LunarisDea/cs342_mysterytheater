52e78f1f7e501102b95d2115b2febf8c7bf24fea
pBengt2 <pbengt2@uic.edu>
2017-03-28 07:05:00 -0500

    Update GameInfo.java

--- Code/GameInfo.java
+++ Code/GameInfo.java
@@ -3,8 +3,15 @@ public interface GameInfo{
 	public static final int INTERNAL_HEIGHT = 360;
 	public static final int DEFAULT_HEIGHT = 64;
 	public static final int DEFAULT_WIDTH = 64;	
+	
+	public static final int UP = 0;
+	public static final int RIGHT = 1;
+	public static final int DOWN = 2;
+	public static final int LEFT = 3;	
 
 	public class Global{
 		public static int size = 1;
-	}	
+	}
+	
+	public enum Direction{ UP, RIGHT, DOWN, LEFT }
 }
